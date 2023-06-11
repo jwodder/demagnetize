75bdbf3ac8b322cf08dfd7f399eedecedb467b41
John T. Wodder II <git@varonathe.org>
2023-06-11 16:44:00 +0000

    Remove debugging commands

--- src/demagnetize/__main__.py
+++ src/demagnetize/__main__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-import json
 import logging
-from pathlib import Path
 from typing import Optional, TextIO
 import anyio
 import click
@@ -9,9 +7,7 @@ from click_loglevel import LogLevel
 import colorlog
 from torf import Magnet
 from .core import Demagnetizer
-from .peer import Peer
-from .trackers import Tracker
-from .util import TRACE, InfoHash, log, yield_lines
+from .util import TRACE, log, yield_lines
 
 
 def validate_template(
@@ -88,47 +84,6 @@ def batch(ctx: click.Context, magnetfile: TextIO, outfile: str) -> None:
         "%d/%d magnet URLs successfully converted to torrent files", r.finished, r.total
     )
     if not r.ok:
-        ctx.exit(1)
-
-
-@main.command()
-@click.argument("tracker", type=Tracker.from_url)
-@click.argument("infohash", type=InfoHash.from_string)
-def get_peers(tracker: Tracker, infohash: InfoHash) -> None:
-    demagnetizer = Demagnetizer()
-    peers = anyio.run(demagnetizer.get_peers_from_tracker, tracker, infohash)
-    for p in peers:
-        print(json.dumps(p.for_json()))
-
-
-@main.command()
-@click.argument("host")
-@click.argument("port", type=int)
-@click.argument("infohash", type=InfoHash.from_string)
-def get_info_from_peer(host: str, port: int, infohash: InfoHash) -> None:
-    demagnetizer = Demagnetizer()
-    peer = Peer(host=host, port=port)
-    info = anyio.run(demagnetizer.get_info_from_peer, peer, infohash)
-    p = Path(f"{infohash}.bencode")
-    log.info("Saving info to %s", p)
-    p.write_bytes(info)
-
-
-@main.command()
-@click.argument("magnet", type=Magnet.from_string)
-@click.pass_context
-def get_magnet_peers(ctx: click.Context, magnet: Magnet) -> None:
-    demagnetizer = Demagnetizer()
-
-    async def aprint_magnet_peers() -> bool:
-        ok = False
-        async with demagnetizer.get_peers_for_magnet(magnet) as ait:
-            async for p in ait:
-                ok = True
-                print(json.dumps(p.for_json()))
-        return ok
-
-    if not anyio.run(aprint_magnet_peers):
         ctx.exit(1)
 
 
